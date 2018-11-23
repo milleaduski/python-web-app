4124e5140cdc9df4e1cb84536ee9263cb8a14e97
millea <millea1994@gmail.com>
2018-11-23 22:37:14 +0700

    add templating and post method

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/welcome')
 def myFunction():
-		return 'REstart server'
+		return render_template('index.html');
 
 @app.route('/welcome/<user>')
 def paramFunc(user):
-		return 'Welcome %s' %user
+		return render_template('profile.html', userData = user)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+	if request.method == "POST":
+		return "Your email is "+ request.form['email']
+	
+	return render_template('login.html');
 
 @app.route('/profile/<int:user_id>')
 def profileFunc(user_id):
