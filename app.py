3fea063e8912651cfc52997a501aab3b630e36b1
millea <millea1994@gmail.com>
2018-12-05 23:03:30 +0700

    add session flask and login system

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, session, url_for, redirect
 from pprint import pprint
 
 app = Flask(__name__)
 
+app.secret_key = '34ujeifaj21mkll'
 @app.route('/welcome')
 def homeFunction():
 		email = request.cookies.get('email_user')
@@ -16,14 +17,24 @@ def paramFunc(user):
 @app.route('/login', methods=["GET", "POST"])
 def login():
 	if request.method == "POST":
-		resp = make_response("Your email is "+ request.form['email'])
-		resp.set_cookie('email_user', request.form['email'])
-		return resp
+		# resp = make_response("Your email is "+ request.form['email'])
+		# resp.set_cookie('email_user', request.form['email'])
+		session['email'] = request.form['email']
+		# return resp
+
+	if 'email' in session:
+		email = session['email']
+		return redirect(url_for('profileFunc', email = email))
 	return render_template('login.html')
 
-@app.route('/profile/<int:user_id>')
-def profileFunc(user_id):
-		return 'Your id is %d' %user_id
+@app.route('/profile/<email>')
+def profileFunc(email):
+	return render_template('profile.html', email = email)
+
+@app.route('/logout')
+def logout():
+	session.pop('email', None)
+	return redirect(url_for("login"))
 
 @app.route('/profile')
 def profFunc():
